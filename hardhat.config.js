4fe8b544bed361d6c53f2cf181c55a5ce926a9e3
Tajammal <tajamul676@gmail.com>
2022-09-29 05:09:45 +0500

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,77 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
+const GOERLI_RPC_URL =
+  process.env.GOERLI_RPC_URL ||
+  "https://eth-mainnet.alchemyapi.io/v2/your-api-key";
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+    },
+    localhost: {
+      chainId: 31337,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+      goerli: ETHERSCAN_API_KEY,
+    },
+  },
+  gasReporter: {
+    enabled: false,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: "ETH",
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.8",
+      },
+      {
+        version: "0.6.12",
+      },
+      {
+        version: "0.6.6",
+      },
+      {
+        version: "0.4.19",
+      },
+      {
+        version: "0.6.0",
+      },
+    ],
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+};
